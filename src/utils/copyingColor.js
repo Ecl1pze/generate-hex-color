da2ce467db414be5b0481c18e2e4d9adf7762973
Ecl1pze <osnovasivtsev@yandex.ru>
2024-06-20 10:55:15 +0300

    Refactoring code

--- src/utils/copyingColor.js
+++ src/utils/copyingColor.js
@@ -1,16 +1,15 @@
-const message = document.querySelector('.message')
-
 export const copyingColor = (text) => {
 	
 	navigator.clipboard.writeText(text).then(
 		() => {
-			const message = generateMessage('Текст скопирован', './src/assets/images/sucsess.svg', 'sucs')
-			setTimeout(() => {document.querySelector('.message').remove()}, 1000)
+			const message = generateMessage('Текст скопирован', './src/assets/images/sucsess.svg', 'sucs');
+			setTimeout(() => {message.remove()}, 1000)
 		}
 	).
 	catch(error => {
+		console.log(error)
 		const message = generateMessage('Текст не скопирован', './src/assets/images/error.svg', 'err')
-		setTimeout(() => {document.querySelector('.message').remove()}, 1000)
+		setTimeout(() => {message.remove()}, 1000)
 	});
 }
 
@@ -22,4 +21,5 @@ function generateMessage(text, image, type) {
 	<p>${text}</p>
 	`;
 	document.body.appendChild(div);
+	return div;
 }
